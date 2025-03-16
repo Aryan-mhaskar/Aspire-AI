d16edb1ee6ed848242cc212cfd5823711ea8d00b
Aniket <aniketpawar112003@gmail.com>
2025-03-16 17:29:11 +0530

    Fix deployment issues

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    // Add any specific rules here
+  }
+};
