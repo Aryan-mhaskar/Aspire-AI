2adc2ab3698161e596d38dd5f7a841592d68e816
Aniket <aniketpawar112003@gmail.com>
2025-09-16 00:13:53 +0530

    Initial commit

--- components/hero.jsx
+++ components/hero.jsx
@@ -53,11 +53,7 @@ const HeroSection = () => {
   Get Started
 </Button>
              </Link>
-             {/* <Link href="https://www.youtube.com/roadsidecoder">
-               <Button size="lg" variant="outline" className="px-8">
-                 Watch Demo
-               </Button>  
-             </Link> */}
+            
           </div>
           <div className="hero-image-wrapper mt-5 md:mt-0 mb-10">
           <div ref={imageRef} className="hero-image">
